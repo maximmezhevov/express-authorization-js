6377eb03a08ddb6a983fe8408e15f07651de9621
maximmezhevov <maximmezhevov@gmail.com>
2025-04-14 02:01:59 +0300

    .

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import express, { type Express } from 'express'
-=======
-import express, { Express } from 'express'
->>>>>>> 3bced7f89fde9f0eb0fbe85a6500bab3c9e2f4e1
 import path from 'path'
 
 export const configureServer = (app: Express) => {
