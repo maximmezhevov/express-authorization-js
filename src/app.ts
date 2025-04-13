2f9920e904300b2db42911c834c636fa3db20847
maximmezhevov <maximmezhevov@gmail.com>
2025-04-13 23:28:44 +0300

    add: TypeScript

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express()
+
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+	res.json({ message: 'Hello, TypeScript Express!' })
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`)
+})
