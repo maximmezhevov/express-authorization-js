0816f3de6bbe33a2466730be0c441aa39f256818
maximmezhevov <maximmezhevov@gmail.com>
2025-04-14 05:02:49 +0300

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,22 @@ import { configureSwagger } from './config/swagger.config'
 import todosRoutes from './routes/todos.route'
 
 export const createApp = (): Express => {
+	console.log('Creating Express app...')
 	const app: Express = express()
+	
+	console.log('Configuring server...')
 	configureServer(app)
+	
+	console.log('Configuring Swagger...')
 	configureSwagger(app)
 
+	console.log('Setting up routes...')
 	app.use('/api', todosRoutes)
 
+	app.get('/', (req, res) => {
+		res.json({ status: 'Server is running' })
+	})
+
+	console.log('App setup completed')
 	return app
 }
