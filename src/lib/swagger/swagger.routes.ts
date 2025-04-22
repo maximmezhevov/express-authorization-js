337a47833095a42cc032a10eee26b4ae7fe023fc
maximmezhevov <maximmezhevov@gmail.com>
2025-04-22 14:36:50 +0300

    3

--- src/lib/swagger/swagger.routes.ts
+++ src/lib/swagger/swagger.routes.ts
@@ -1,23 +1,19 @@
 // src/routes/swagger.routes.ts
 import { Router } from 'express'
 import swaggerUi from 'swagger-ui-express'
-import path from 'path'
 import { swaggerJsdocOptions, swaggerUiOptions } from './swagger.config'
-import express from 'express'
 
 const router = Router()
-
-// Serve Swagger UI static files
-router.use(
-	'/swagger-ui',
-	express.static(path.join(__dirname, '../../node_modules/swagger-ui-dist'))
-)
 
 // Serve Swagger UI
 router.use(
 	'/api-docs',
 	swaggerUi.serve,
-	swaggerUi.setup(swaggerJsdocOptions, swaggerUiOptions)
+	swaggerUi.setup(swaggerJsdocOptions, {
+		...swaggerUiOptions,
+		customSiteTitle: 'Todo API Documentation',
+		customfavIcon: '/favicon.ico'
+	})
 )
 
 // Serve Swagger JSON
